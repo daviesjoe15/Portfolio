b481a657fb20c2b71dcac96dc75c8fd699d341a2
Joe Davies <you@example.comdaviesjoe15@gmail.com>
2019-04-09 16:21:46 -0700

    try to fix slide show 2

--- js/main.js
+++ js/main.js
@@ -33,31 +33,3 @@ function testDocReady() {
 
 
 
-// Tabbed images
-
-  var slideIndex = 1;
-    show1Slides(slideIndex);
-
-    function plusSlides(n) {
-      show1Slides(slideIndex += n);
-    }
-
-    function currentSlide(n) {
-      show1Slides(slideIndex = n);
-    }
-
-    function show1Slides(n) {
-      var i;
-      var slides = document.getElementsByClassName("mySlides", "mySlides2");
-      var dots = document.getElementsByClassName("image-tab");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";  
-      }
-      for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex-1].style.display = "block";  
-      dots[slideIndex-1].className += " active";
-    }
