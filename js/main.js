28387af8e449bdf44cfe1a6bbf8b8da07a392cfc
Joe Davies <you@example.comdaviesjoe15@gmail.com>
2019-04-09 17:02:29 -0700

    added carousel as component

--- js/main.js
+++ js/main.js
@@ -33,3 +33,31 @@ function testDocReady() {
 
 
 
+// Tabbed images
+
+  var slideIndex = 1;
+    show1Slides(slideIndex);
+
+    function plusSlides(n) {
+      show1Slides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+      show1Slides(slideIndex = n);
+    }
+
+    function show1Slides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("image-tab");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    }
